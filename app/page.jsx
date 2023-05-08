d3f8b2beb40b1076afc66dbc2422681fc50c9086
LucasTisera <lucastisera02@gmail.com>
2023-05-08 16:58:53 -0300

    Feat - Visor de wondows imagenes, al home, y foto de portada, estructuracion de componentes para que quede modular

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,24 @@
 import styles from './HomePage.module.css'
+import { WindowsImageViewer } from '@/components/ImageContainer'
+
+
 
 export default function HomePage () {
   return(
     <section className={styles.home}>
-      <div className={styles.homeContent}>
-        <h1>Hi, I'm <span>Lucas Tisera</span></h1>
-        <div className={styles.textAnimate}>
-          <h3>Front End Developer</h3>
+      <div className={styles.contentContainer}>
+        <div className={styles.homeContent}>
+          <h1>Hi, I'm <span>Lucas Tisera</span></h1>
+          <div className={styles.textAnimate}>
+            <h3>Full Stack Developer</h3>
+          </div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium excepturi dolorem et vero odit ipsa delectus nam. Voluptatem perspiciatis molestiae qui laudantium omnis corporis fugit numquam! Facilis eos obcaecati alias!</p>
+          <div className={styles.btnContainer}>
+            <a href="#" className={styles.btn}>Hire Me</a>
+            <a href="#" className={styles.btn}>Let's Talk</a>
+          </div>
         </div>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium excepturi dolorem et vero odit ipsa delectus nam. Voluptatem perspiciatis molestiae qui laudantium omnis corporis fugit numquam! Facilis eos obcaecati alias!</p>
-        <div className={styles.btnContainer}>
-          <a href="#" className={styles.btn}>Hire Me</a>
-          <a href="#" className={styles.btn}>Let's Talk</a>
-        </div>
+        <WindowsImageViewer/>
       </div>
     </section>
   )
---
+++ components/ImageContainer.jsx
@@ -0,0 +1,44 @@
+import styles from './styles/ImageContainer.module.css'
+import Image from 'next/image'
+import mypic from '../public/myPictureFocusedAgain2.png'
+export function WindowsImageViewer(){
+    return(
+        <section className={styles.pictureContainer}>
+            <div className={styles.navContainer}>
+                <ul className={styles.nav}>
+                    <li className={styles.navItemMenu}>
+                        <div class='bx bx-images'></div>
+                    </li>
+                    <li className={styles.navItemName}>
+                        <p>PictureUsedInMyPort...</p>
+                    </li>
+                    <li className={styles.navItemControls}>
+                        <div> ...</div>
+                    </li>
+                    <li className={styles.navItemControls}>
+                        <div class='bx bx-zoom-out'></div>
+                    </li>
+                    <li className={styles.navItemControls}>
+                        <div class='bx bx-zoom-in'></div>
+                    </li>
+                    <li className={styles.navItemPorcent}>
+                        <p>45 %</p>
+                    </li>
+                    <div className={styles.functionControllers}>
+                        <li className={styles.navItemFunction}>
+                        <div class='bx bx-minus'></div>
+
+                        </li>
+                        <li className={styles.navItemFunction}>
+                            <div class='bx bx-checkbox'></div>
+                        </li>
+                        <li className={styles.navItemFunctionX}>
+                            <div class='bx bx-x'></div>
+                        </li>
+                    </div>
+                </ul>
+            </div>
+            <Image src={mypic} alt="Me" className={styles.picture} />
+        </section>
+    )
+}
