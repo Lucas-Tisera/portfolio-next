801e21b632ba2d7de3fa5bb67566b25a50b46fd3
LucasTisera <lucastisera02@gmail.com>
2023-05-10 13:42:30 -0300

    Fix - apos

--- app/page.jsx
+++ app/page.jsx
@@ -11,14 +11,14 @@ export default function HomePage () {
     <section className={styles.home}>
       <div className={styles.contentContainer}>
         <div className={styles.homeContent}>
-          <h1>Hi, I `&apos;` m <span>Lucas Tisera</span></h1>
+          <h1>Hi, I&apos;m <span>Lucas Tisera</span></h1>
           <div className={styles.textAnimate}>
             <h3>Full Stack Developer</h3>
           </div>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium excepturi dolorem et vero odit ipsa delectus nam. Voluptatem perspiciatis molestiae qui laudantium omnis corporis fugit numquam! Facilis eos obcaecati alias!</p>
           <div className={styles.btnContainer}>
             <a href="#" className={styles.btn}>Hire Me</a>
-            <a href="#" className={styles.btn}>Let`&apos;`s Talk</a>
+            <a href="#" className={styles.btn}>Let&apos;s Talk</a>
           </div>
             <RedesLink/>
         </div>
