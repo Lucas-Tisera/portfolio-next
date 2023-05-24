323b739a1bcafbae7a14a491649faf8a27bdde1d
LucasTisera <lucastisera02@gmail.com>
2023-05-24 12:37:53 -0300

    FEAT/FIX - fonts navbar y cambios en skills cards

--- components/SkillsView.jsx
+++ components/SkillsView.jsx
@@ -14,40 +14,70 @@ export function SkillsResume(){
     return (
         <section style={{marginTop:"100px"}}>
             <div className={styles.title}>
-
                 <h1 >
                     Tech Skills
                 </h1>
             </div>
+
             <div className={styles.skillsContainer}>
+                
                 <div className={styles.row}>
-                    <div className={styles.itemContent} style={{borderBottom: "2px solid #F25320"}}>
-                        <div className='bx bxl-html5' style={{color:"#F25320"}}></div>
-                        <h3>HTML5</h3>
+                    <div className={styles.card}>
+                        <div className={styles.front} >
+                            <div className='bx bxl-html5' style={{color:"#F25320"}}></div>
+                        </div>
+                        <div className={styles.back} >
+                            <h3>HTML</h3>
+                        </div>
                     </div>
-                    <div className={styles.itemContent} style={{borderBottom: "2px solid #2071F2"}}>
-                        <div className='bx bxl-css3' style={{color:"#2071F2"}}></div>
-                        <h3>CSS3</h3>
+                    <div className={styles.card}>
+                        <div className={styles.front}>
+                            <div className='bx bxl-css3' style={{color:"#2071F2"}}></div>
+
+                        </div>
+                        <div className={styles.back} >
+                            <h3>CSS</h3>
+                        </div>
                     </div>
-                    <div className={styles.itemContent} style={{borderBottom: "2px solid #EAD41C"}}>
-                        <div className='bx bxl-javascript' style={{color:"#EAD41C"}}></div>
-                        <h3>JavaScript</h3>
+                    <div className={`${styles.card} ${styles.main}`}>
+                        <div className={styles.front} >
+                            <div className='bx bxl-javascript' style={{color:"#EAD41C"}}></div>
+
+                        </div>
+                        <div className={styles.back} >
+                            <h3>JAVASCRIPT</h3>
+                        </div>
                     </div>
+                    
                 </div>
                 <div className={styles.row}>
-                    <div className={styles.itemContent} style={{borderBottom: "2px solid #46CAF2"}}>
-                        <div className='bx bxl-react' style={{color:"#46CAF2"}}></div>
-                        <h3>React.js</h3>
-                    </div>
-                    <div className={styles.itemContent} style={{borderBottom: "2px solid #fff"}}>
-                        <div>
-                            <Image src='/static/next.svg' width={70} height={70} alt='nextjs' className={styles.nextIcon}/>
+                    <div className={`${styles.card} ${styles.main}`}>
+                        <div className={styles.front} >
+                            <div className='bx bxl-react' style={{color:"#46CAF2"}}></div>
                         </div>
-                        <h3>Next.js</h3>
+                        <div className={styles.back} >
+                            <h3>REACT.JS</h3>
+                        </div>
                     </div>
-                    <div className={styles.itemContent} style={{borderBottom: "2px solid #89BB3C"}}>
-                        <div className='bx bxl-nodejs' style={{color:"#89BB3C"}}></div>
-                        <h3>NodeJS</h3>
+                    <div className={styles.card}>
+                        <div className={styles.front} >
+                            <div>
+                                <Image src='/static/next.svg' width={70} height={70} alt='nextjs' className={styles.nextIcon}/>
+                            </div>
+
+                        </div>
+                        <div className={styles.back} >
+                            <h3>NEXT.JS</h3>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.front} >
+                            <div className='bx bxl-nodejs' style={{color:"#89BB3C"}}></div>
+
+                        </div>
+                        <div className={styles.back} >
+                            <h3>NODE.JS</h3>
+                        </div>
                     </div>
                 </div>
             </div>
